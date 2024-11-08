bb1fc3b9be86c1baaa7835b6b03947942021e7cf
CHYHEE <wnn2011@naver.com>
2024-11-08 15:06:54 +0900

    11/08

---
+++ src/step1/Diff.java
@@ -0,0 +1,13 @@
+package step1;
+
+import java.util.Scanner;
+
+public class Diff {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a - b);
+    }
+}
