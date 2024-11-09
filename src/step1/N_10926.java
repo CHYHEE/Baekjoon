9b20c3a98671e206f85fefd9bbd362232c725127
CHYHEE <wnn2011@naver.com>
2024-11-09 23:42:23 +0900

    11/09

---
+++ src/step1/N_10926.java
@@ -0,0 +1,12 @@
+package step1;
+
+import java.util.Scanner;
+
+public class N_10926 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String id = scanner.nextLine();
+        System.out.println(id + "??!");
+    }
+}
---
+++ src/step1/YearCalculation.java
@@ -0,0 +1,14 @@
+package step1;
+
+import java.util.Scanner;
+
+public class YearCalculation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int yearB = scanner.nextInt();
+        int yearA = yearB-543;
+        System.out.println(yearA);
+
+    }
+}
