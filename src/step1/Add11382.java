4fbc008c6b32ab72b59ae9f2fb68c4c57c016390
CHYHEE <wnn2011@naver.com>
2024-11-13 22:06:51 +0900

    11/13

---
+++ src/step1/Add11382.java
@@ -0,0 +1,16 @@
+package step1;
+
+import java.util.Scanner;
+
+public class Add11382 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //조건: A, B, C (1 ≤ A, B, C ≤ 1012)
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        long c = scanner.nextLong();
+
+        System.out.println(a + b + c);
+    }
+}
--- src/step1/Mul2588.java
+++ src/step1/Mul2588.java
@@ -9,8 +9,11 @@ public class Mul2588 {
         int num1 = scanner.nextInt();
         int num2 = scanner.nextInt();
 
+        //일의 자리 숫자를 구하는 표현
         System.out.println(num1 * (num2 % 10));
+        //십의 자리 숫자를 구하는 표현
         System.out.println(num1 * (num2 % 100 / 10));
+        //백의 자리 숫자를 구하는 표현
         System.out.println(num1 * (num2 / 100));
         System.out.println(num1 *  num2);
     }
