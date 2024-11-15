342d93ad2884d07ee4bf2f62714b1562bb33b79d
CHYHEE <wnn2011@naver.com>
2024-11-15 16:07:23 +0900

    11/15

---
+++ src/step2/Comparison.java
@@ -0,0 +1,20 @@
+package step2;
+
+import java.util.Scanner;
+
+public class Comparison {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if (a > b) {
+            System.out.println(">");
+        } else if (a < b) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
