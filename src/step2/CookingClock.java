29471a6e51a346397626ceb8d84d51b9c3749d7a
CHYHEE <wnn2011@naver.com>
2024-11-20 13:51:40 +0900

    11/20_시계

---
+++ src/step2/CookingClock.java
@@ -0,0 +1,21 @@
+package step2;
+
+import java.util.Scanner;
+
+public class CookingClock {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int min = 60 * a + b;
+        min += c;
+
+        int hour = (min / 60) % 24;
+        int minute = min % 60;
+
+        System.out.println(hour + " " + minute);
+    }
+}
--- src/step2/Quadrant.java
+++ src/step2/Quadrant.java
@@ -2,7 +2,7 @@ package step2;
 
 import java.util.Scanner;
 
-public class Num4 {
+public class Quadrant {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
