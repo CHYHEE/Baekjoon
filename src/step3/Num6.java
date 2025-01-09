f0883e3eac492ad76b18bc0faee4cd126cc55224
CHYHEE <wnn2011@naver.com>
2025-01-09 14:12:21 +0900

    01/09-빠른 A+B(복습필요)

---
+++ src/step3/Num6.java
@@ -0,0 +1,27 @@
+package step3;
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.StringTokenizer;
+
+public class Num6 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            bw.write((a + b) + "\n");
+        }
+
+        bw.flush();
+
+    }
+}
