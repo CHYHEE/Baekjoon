05bd0a03125b03a059ce483589e4996fc4c70424
CHYHEE <wnn2011@naver.com>
2025-01-15 14:16:36 +0900

    01/15-A+B-5

---
+++ src/step3/Num11.java
@@ -0,0 +1,22 @@
+package step3;
+
+import java.util.Scanner;
+
+public class Num11 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            if (a == 0 && b == 0) {
+                break;
+            }
+
+            System.out.println(a+b);
+        }
+
+    }
+}
