f6c684c2a1c0f7af3619c198b90406459b919160
CHYHEE <wnn2011@naver.com>
2025-01-21 23:59:56 +0900

    01/21-공 넣기(10810)복습

--- src/step4/Num5.java
+++ src/step4/Num5.java
@@ -2,26 +2,28 @@ package step4;
 import java.util.Scanner;
 
 public class Num5 {
-        public static void main(String[] args) {
 
-            Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
 
-            int N = sc.nextInt();
-            int M = sc.nextInt();
-            int[] temp = new int[N];
+        Scanner sc = new Scanner(System.in);
 
-            for(int i = 0; i < M; i++) {
-                int a = sc.nextInt();
-                int b = sc.nextInt();
-                int c = sc.nextInt();
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] temp = new int[N];
 
-                for(int j = a - 1; j < b; j++) {
-                    temp[j] = c;
-                }
-            }
-            for(int k = 0; k < temp.length; k++) {
-                System.out.print(temp[k] + " ");
+        for (int i = 0; i < M; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            for (int j = a - 1; j < b; j++) {
+                temp[j] = c;
             }
         }
+
+        for (int k = 0; k < temp.length; k++) {
+            System.out.print(temp[k] + " ");
+        }
     }
+}
 
