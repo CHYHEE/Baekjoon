62878e95d68b1d07834527cfc78633bbfdb299b5
CHYHEE <wnn2011@naver.com>
2025-01-22 19:49:40 +0900

    01/21-공 넣기(10810)복습

--- src/step4/Num5.java
+++ src/step4/Num5.java
@@ -4,25 +4,25 @@ import java.util.Scanner;
 public class Num5 {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int[] temp = new int[N];
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
 
-        for (int i = 0; i < M; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
+        for (int i = 0; i < m; i++) {
+            int I = sc.nextInt();
+            int J = sc.nextInt();
+            int K = sc.nextInt();
 
-            for (int j = a - 1; j < b; j++) {
-                temp[j] = c;
+            //배열의 인덱스는 0부터 시작하지만 입력값은 1부터 시작하는 값이기 때문에 I-1을 해준다.
+            for (int j = I - 1; j < J; j++) {
+                arr[j] = K;
             }
         }
 
-        for (int k = 0; k < temp.length; k++) {
-            System.out.print(temp[k] + " ");
+        for (int k = 0; k < arr.length; k++) {
+            System.out.print(arr[k] + " ");
         }
     }
 }
