cd5fb64fcab7fef40720bb25873541b005eea398
CHYHEE <wnn2011@naver.com>
2025-02-04 22:10:58 +0900

    02/04-단어 길이 재기(2743)

---
+++ src/step5/Num2.java
@@ -0,0 +1,14 @@
+package step5;
+
+import java.util.Scanner;
+
+public class Num2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        System.out.println(s.length());
+    }
+}
