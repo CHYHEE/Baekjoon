b89983b2ed039c1d1142af7dd6f696a8756a37f4
CHYHEE <wnn2011@naver.com>
2025-02-10 22:14:06 +0900

    02/10-숫자의 합(11720) 복습

---
+++ src/step5/Num5.java
@@ -0,0 +1,20 @@
+package step5;
+
+import java.util.Scanner;
+
+public class Num5 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String str = sc.next();
+        int sum = 0;
+
+        for(int i=0;i<n;i++) {
+            sum += str.charAt(i) - '0'; // '0'을 하면 문자를 정수로 변환할 수 있다.
+        }
+
+        System.out.println(sum);
+    }
+}
